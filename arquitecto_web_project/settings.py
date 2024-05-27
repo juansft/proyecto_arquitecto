c4f7468449e800b4ac8e1f721241d059b24ba95c
Juan <juandita333333@gmail.com>
2024-05-27 18:08:46 -0500

    3

--- arquitecto_web_project/settings.py
+++ arquitecto_web_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-^s-g7rbnm@8oqe)uzs3ibfb)u4pnadr821$!fqk_h*hv!0s_nn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['arquitecto.azurewebsites.net']
+ALLOWED_HOSTS = ['arquitecto.azurewebsites.net', '127.0.0.1']
 
 
 # Application definition
